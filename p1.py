5a759bf1dde94ac66ae1c3b60e8774a83992e38b
FernandaPerezV <fer20mc@gmail.com>
2015-11-22 17:57:39 -0300

    se borra solucion con ajuste lineal y se realiza con bootstrap con H0 como division de sumatorias (al minimizar funcion chi cuadrado)

--- p1.py
+++ p1.py
@@ -1,19 +1,50 @@
+from __future__ import division
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
 
 # importando los datos
 file_path = os.path.join('data', 'hubble_original.dat')
 distancias = np.loadtxt(fname=file_path, usecols=(0,))
 velocidades = np.loadtxt(fname=file_path, usecols=(1,))
 
+# H0 = suma(vi*di) / suma(di)
+numerador = 0
+denominador = 0
+for i in range(len(distancias)):
+    numerador += velocidades[i] * distancias[i]
+    denominador += distancias[i] * distancias[i]
+H0 = numerador / denominador
+print H0
+
+# buscando intervalo de confianza al 95
+# simulacion bootstrap
+np.random.seed(1234)
+N = 24
+Nboot = 500
+values = np.zeros(Nboot)
+
+for i in range(Nboot):
+    s = np.random.randint(low=0, high=N, size=N)
+    num = 0
+    den = 0
+    for j in s:
+        num += velocidades[j] * distancias[j]
+        den += distancias[j] * distancias[j]
+    values[i] = np.mean(num / den)
+
+values_ordenados = np.sort(values)
+limite_bajo = values_ordenados[int(Nboot * 0.03)]
+limite_alto = values_ordenados[int(Nboot * 0.98)]
+print "El intervalo de confianza al 95% es: [{}:{}]".format(limite_bajo, limite_alto)
+
+# plot
 fig = plt.figure(1)
 fig.clf()
-
-ax = fig.add_subplot(111)
-ax.set_xlabel('Distancia [$Mpc$]')
-ax.set_ylabel('Velocidad [$km/s$]')
-plt.title("Datos originales Hubble")
-plt.plot(distancias, velocidades, 'o')
+plt.hist(values, bins=50)
+plt.axvline(H0, color='r')
 plt.draw()
 plt.show()
+plt.savefig('bootstrap.png')
