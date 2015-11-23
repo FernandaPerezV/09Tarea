8283f174106551411877be76e8f753df8ef975c8
FernandaPerezV <fer20mc@gmail.com>
2015-11-23 15:30:42 -0300

    se grafican puntos con barras de error y recta, y se calculan intervalos de confianza para pendiente y coefs de posicion, y se realizan histogramas

--- p3.py
+++ p3.py
@@ -14,35 +14,55 @@ error_z = np.loadtxt(fname=file_path, usecols=(83,)) * 3.631
 
 # montecarlo
 np.random.seed(1234)
-Nmc = 100
-mean_values_i = np.zeros(Nmc)
-mean_values_z = np.zeros(Nmc)
+Nmc = 10000
+pendientes = np.zeros(Nmc)
+coefs_posicion = np.zeros(Nmc)
 for i in range(Nmc):
     r = np.random.normal(0, 1, size=len(flujo_i))
     muestra_i = flujo_i + error_i * r
-    mean_values_i[i] = np.mean(muestra_i)
     muestra_z = flujo_z + error_z * r
-    mean_values_z[i] = np.mean(muestra_z)
+    pendientes[i], coefs_posicion[i] = np.polyfit(muestra_i, muestra_z, 1)
 
-[a, b] = np.polyfit(mean_values_i, mean_values_z, 1)
-[c, d] = np.polyfit(flujo_i, flujo_z, 1)
-#x = np.linspace(0,450,1000)
 
-fig = plt.figure(1)
-fig.clf()
-plt.plot(flujo_i, flujo_z, 'o')
-plt.errorbar(flujo_i, flujo_z, yerr=error_z, xerr=error_i,
-             marker='.', ls='None', capsize=0.,color='g')
-#plt.plot(x, b + a * x)
-#plt.plot(x, d + c * x)
+# plot 1
+fig1 = plt.figure(1)
+fig1.clf()
+plt.hist(coefs_posicion, bins=30)
 plt.draw()
 plt.show()
 
-'''
+# plot 2
 fig2 = plt.figure(2)
 fig2.clf()
-plt.hist(mean_values_i/mean_values_z, bins=30)
-#plt.axvline(b, color='r')
+plt.hist(pendientes, bins=30)
 plt.draw()
 plt.show()
-'''
+
+# plot 3
+a, b = np.polyfit(flujo_i, flujo_z, 1)
+x = np.linspace(-100,500,1000)
+fig3 = plt.figure(3)
+fig3.clf()
+plt.errorbar(flujo_i, flujo_z, xerr=error_i, yerr=error_z, fmt="o",
+                 label="bla")
+plt.plot(x, a * x + b)
+plt.plot()
+plt.draw()
+plt.show()
+
+
+
+
+# calculando intervalo de confianza
+pendientes_ord = np.sort(pendientes)
+coefs_posicion_ord = np.sort(coefs_posicion)
+limite_bajo_1 = pendientes_ord[int(Nmc * 0.03)]
+limite_alto_1 = pendientes_ord[int(Nmc * 0.98)]
+limite_bajo_2 = coefs_posicion_ord[int(Nmc * 0.03)]
+limite_alto_2 = coefs_posicion_ord[int(Nmc * 0.98)]
+print """El intervalo de confianza al
+             95% para la pendiente es: [{}:{}]""".format(limite_bajo_1,
+                                                         limite_alto_1)
+print """El intervalo de confianza al
+             95% para el coef de posicion es: [{}:{}]""".format(limite_bajo_2,
+                                                                limite_alto_2)
